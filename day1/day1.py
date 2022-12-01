312c50e3be1b36adf6769f250131bdd3ecd79e6f
Anno Knierim <anno.knierim@tu-dortmund.de>
2022-12-01 15:35:19 +0100

    Added solution for part 2

--- day1/day1.py
+++ day1/day1.py
@@ -4,17 +4,17 @@
 elves = []
 total_calories = []
 
-with open("data/day1.txt") as f:
+with open("day1/day1.txt") as f:
     lines = f.readlines()
-    
+
     # prerequisites
     calories = 0
     elf_count = 1
 
     for line in lines:
-        # check that the current line is not a new line, 
-        # else save sum over Calories to total_calories 
-        # and save elf id        
+        # check that the current line is not a new line,
+        # else save sum over Calories to total_calories
+        # and save elf id
         if line != "\n":
             calories += int(line)
         else:
@@ -24,7 +24,7 @@ with open("data/day1.txt") as f:
             # reset calories, increase elf id counter
             calories = 0
             elf_count += 1
-    
+
     # last elf in list
     total_calories.append(calories)
     elves.append(elf_count)
@@ -34,7 +34,12 @@ most_calories = max(total_calories)
 elf_id = elves[total_calories.index(most_calories)]
 
 print(
-    f"{'Elf carrying the most Calories:':<31} {elf_id:>10}",
-    f"\n{'Total Calories carried:':<31} {most_calories:>10}"
+    f"{'Elf carrying the most Calories:':<40} {elf_id:>10}",
+    f"\n{'Total Calories carried:':<40} {most_calories:>10}"
 )
 
+# Part 2
+sorted_calories = sorted(total_calories)
+sum_top_three = sum(sorted_calories[-3:])
+
+print(f"{'Sum of Calories of the top three elves:':<40} {sum_top_three:>10}")
