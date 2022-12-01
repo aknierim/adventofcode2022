41fe03ce24cda4519edff95ca8fe50572498dd56
Anno Knierim <anno.knierim@tu-dortmund.de>
2022-12-01 10:50:42 +0100

    Day 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# adventofcode22
+# Advent of Code 2022
+
+My Solutions to the [Advent of Code 2022](https://adventofcode.com/) written in Python.
---
+++ day1/day1.py
@@ -0,0 +1,40 @@
+"""Advent of Code 2022 Day 1 solution.
+"""
+# initialize lists
+elves = []
+total_calories = []
+
+with open("data/day1.txt") as f:
+    lines = f.readlines()
+    
+    # prerequisites
+    calories = 0
+    elf_count = 1
+
+    for line in lines:
+        # check that the current line is not a new line, 
+        # else save sum over Calories to total_calories 
+        # and save elf id        
+        if line != "\n":
+            calories += int(line)
+        else:
+            total_calories.append(calories)
+            elves.append(elf_count)
+
+            # reset calories, increase elf id counter
+            calories = 0
+            elf_count += 1
+    
+    # last elf in list
+    total_calories.append(calories)
+    elves.append(elf_count)
+
+
+most_calories = max(total_calories)
+elf_id = elves[total_calories.index(most_calories)]
+
+print(
+    f"{'Elf carrying the most Calories:':<31} {elf_id:>10}",
+    f"\n{'Total Calories carried:':<31} {most_calories:>10}"
+)
+
