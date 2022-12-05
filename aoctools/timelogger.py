c3e43b44d18c14436e512b5272d959395e6d1fbe
Anno Knierim <anno.knierim@tu-dortmund.de>
2022-12-05 17:29:53 +0100

    Added more colors to aoctools/timelogger.py

--- aoctools/timelogger.py
+++ aoctools/timelogger.py
@@ -1,8 +1,12 @@
+"""Time logger to be used as a decorator for my
+Advent of Code solutions.
+"""
 import logging
 import time
 
 YELLOW = "\x1b[33;20m"
 GREEN = "\033[92m"
+RED = "\033[91m"
 RESET = "\x1b[0m"
 
 logging.basicConfig(
@@ -11,7 +15,18 @@ logging.basicConfig(
 )
 
 
-def timelogger(year: int, day: int, part: int=None):
+def timelogger(year: int=0, day: int=0, part: int=None):
+    """Decorator to time the execution of a function.
+
+    Parameters:
+    -----------
+        year: int
+            The year of the AoC puzzle.
+        day: int
+            The day of the AoC puzzle.
+        part: int
+            The part of the AoC puzzle (optional).
+    """
     def decorator(func):
         def wrapper(*args, **kwargs):
             try:
@@ -19,13 +34,21 @@ def timelogger(year: int, day: int, part: int=None):
                 result = func(*args, **kwargs)
                 end = time.time()
 
+                time_taken = end - start
+                if time_taken < 0.5:
+                    time_taken = GREEN + f"{time_taken*1000:.2f} " + RESET + "ms"
+                elif time_taken < 1:
+                    time_taken = YELLOW + f"{time_taken*1000:.2f} " + RESET + "ms"
+                else:
+                    time_taken = RED + f"{time_taken:.2f} " + RESET + "s"
+
                 if part == None:
                     logging.info(
-                        f"AoC {year}, Day {day}: {func.__name__}() took {end - start:.3f} seconds"
+                        f"AoC {year}, Day {day}: {func.__name__}() took {time_taken}"
                     )
                 else:
                     logging.info(
-                        f"AoC {year}, Day {day} | Part {part}: {func.__name__}() took {end - start:.3f} seconds"
+                        f"AoC {year}, Day {day} | Part {part}: {func.__name__}() took {time_taken}"
                     )
             except Exception as e:
                 logging.error(e)
--- main.py
+++ main.py
@@ -4,39 +4,41 @@ from day3.day3 import day3
 from day4.day4 import day4
 from day5.day5 import day5
 
-elf_id, most_cals, top_three_cals = day1()
-print(
-    "DAY 1:"
-    f"\n{'Elf carrying the most Calories:':<40} {elf_id:>10}",
-    f"\n{'Total Calories carried:':<40} {most_cals:>10}",
-    f"\n{'Sum of Calories of the top three elves:':<40} {top_three_cals:>10}"
-)
+if __name__ == "__main__":
 
-score_p1, score_p2 = day2()
-print(
-    "\nDAY 2:"
-    f"\n{'Score for part 1:':<40} {score_p1:>10}",
-    f"\n{'Score for part 2:':<40} {score_p2:>10}"
-)
+    elf_id, most_cals, top_three_cals = day1()
+    print(
+        "DAY 1:"
+        f"\n{'Elf carrying the most Calories:':<40} {elf_id:>10}",
+        f"\n{'Total Calories carried:':<40} {most_cals:>10}",
+        f"\n{'Sum of Calories of the top three elves:':<40} {top_three_cals:>10}"
+    )
 
-sum_p1, sum_p2 = day3()
-print(
-    "\nDAY 3:",
-    f"\n{f'{chr(931)} priorities of the item types (Part 1):':<40} {sum_p1:>10}",
-    f"\n{f'{chr(931)} priorities of the item types (Part 2):':<40} {sum_p2:>10}"
-)
+    score_p1, score_p2 = day2()
+    print(
+        "DAY 2:"
+        f"\n{'Score for part 1:':<40} {score_p1:>10}",
+        f"\n{'Score for part 2:':<40} {score_p2:>10}"
+    )
 
-ans_p1, ans_p2 = day4()
-print(
-    "\nDAY 4:",
-    f"\n{'Pairs in which one range fully contains':<40}",
-    f"\n{'another:':<40} {ans_p1:>10}",
-    f"\n{'Number of overlapping ranges:':<40} {ans_p2:>10}"
-)
+    sum_p1, sum_p2 = day3()
+    print(
+        "DAY 3:",
+        f"\n{f'{chr(931)} priorities of the item types (Part 1):':<40} {sum_p1:>10}",
+        f"\n{f'{chr(931)} priorities of the item types (Part 2):':<40} {sum_p2:>10}"
+    )
 
-crates_pt1, crates_pt2 = day5()
-print(
-    "\nDAY 5:",
-    f"\n{'Crates on top of each stack (Part 1):':<40} {crates_pt1:>10}",
-    f"\n{'Crates on top of each stack (Part 2):':<40} {crates_pt2:>10}"
-)
+    ans_p1, ans_p2 = day4()
+    print(
+        "DAY 4:",
+        f"\n{'Pairs in which one range fully contains':<40}",
+        f"\n{'another:':<40} {ans_p1:>10}",
+        f"\n{'Number of overlapping ranges:':<40} {ans_p2:>10}"
+    )
+
+    crates_pt1, crates_pt2 = day5()
+    print(
+        "DAY 5:",
+        f"\n{'Crates on top of each stack (Part 1):':<40} {crates_pt1:>10}",
+        f"\n{'Crates on top of each stack (Part 2):':<40} {crates_pt2:>10}"
+    )
